19087c13522c0b535a8addf99c0fde1128d5d852
Adrian Wohlfarter <adrianwohlfarter@gmail.com>
2019-06-26 22:07:25 +0200

    Updates

---
+++ precache-manifest.2056c7a3c1513632e1d06f597abdd203.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c5e37e264c245c57a6549650430561",
+    "url": "/improvisingGen/index.html"
+  },
+  {
+    "revision": "78648681f44626a91d01",
+    "url": "/improvisingGen/static/css/main.93db6b34.chunk.css"
+  },
+  {
+    "revision": "5a59ff517d3b0db0a0f9",
+    "url": "/improvisingGen/static/js/2.04204285.chunk.js"
+  },
+  {
+    "revision": "78648681f44626a91d01",
+    "url": "/improvisingGen/static/js/main.142f43b8.chunk.js"
+  },
+  {
+    "revision": "11882ab9a499940cba23",
+    "url": "/improvisingGen/static/js/runtime~main.4b6c2bcc.js"
+  }
+]);
